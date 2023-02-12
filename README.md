767c9b31844e3458d31da3f49f7c42929bf28262
ThamaraiselviS26 <125075763+ThamaraiselviS26@users.noreply.github.com>
2023-02-12 13:53:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-eeve5n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-eeve5n)
---
+++ src/App2.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <>
+    <div>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+    <div>
+     <p>Start editing to see some magic happen :)</p>
+   </div>
+   </>
+  );
+}
